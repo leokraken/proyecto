35a9d36b49747595e0137b2ca38f4160398b38f7
vale9003 <vale9003@gmail.com>
2015-08-04 19:44:10 -0300

    Cambie color de navbar

--- SAREM.Web/App_Start/BundleConfig.cs
+++ SAREM.Web/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace SAREM.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/custom.css",
                       "~/Content/site.css"));
         }
     }
